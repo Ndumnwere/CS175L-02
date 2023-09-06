b18945ab31fde5cb55634baae20b6b19815d1164
Ndumnwere <144280793+Ndumnwere@users.noreply.github.com>
2023-09-06 16:14:47 -0400

    my information v2

---
+++ myInfo.p.py
@@ -0,0 +1,15 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================ RESTART: C:/Users/chidu/AppData/Local/Programs/Python/Python311/CS175L.py ============================================
+>>> 
+============================================ RESTART: C:/Users/chidu/AppData/Local/Programs/Python/Python311/CS175L.py ============================================
+Ezinne Ndumnwere
+I live at:33 Waterworks road, Abakaliki, Ebonyi,405208
+My phone number is: 803-615-0577
+>>> 
+============================================ RESTART: C:/Users/chidu/AppData/Local/Programs/Python/Python311/CS175L.py ============================================
+Ezinne Ndumnwere
+I live at:33 Waterworks road, Abakaliki, Ebonyi,405208
+My phone number is: 803-615-0577
+My major is : Information System
